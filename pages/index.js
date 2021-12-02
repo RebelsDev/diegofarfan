b7008cb8dd82ee5e1794767b4bf2910b36b265e1
Renzo Damian <renzo.damian.go@gmail.com>
2021-12-02 16:40:03 -0500

    beneficios

--- pages/index.js
+++ pages/index.js
@@ -44,8 +44,8 @@ export default function Home() {
             <h2>
               si eres dueño de una marca o tienes una marca personal, probablemente...
             </h2>
-            <div className={styles.beneficios_elements}>
-              <div className={styles.beneficios_element}>
+            <div className={styles.miedos_elements}>
+              <div className={styles.miedos_element}>
                 <img src="/img/contenido.png" alt="contenido logo" />
 
                 <p>
@@ -53,7 +53,7 @@ export default function Home() {
 
                 </p>
               </div>
-              <div className={styles.beneficios_element}>
+              <div className={styles.miedos_element}>
                 <img src="/img/camara.png" alt="camara logo" />
                 {/* <svg id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 143.97 143.97"><defs><style>.cls-1{fill:none;}.cls-2{opacity:0.49;}.cls-3{fill:#ffbc00;}.cls-4{clip - path:url(#clip-path);}</style><clipPath id="clip-path" transform="translate(-157.58 -1595.63)"><rect class="cls-1" x="198.09" y="1629.03" width="64.07" height="64.07" /></clipPath></defs><circle class="cls-3" cx="71.98" cy="71.98" r="71.98" /></svg> */}
                 <p>
@@ -61,7 +61,7 @@ export default function Home() {
 
                 </p>
               </div>
-              <div className={styles.beneficios_element}>
+              <div className={styles.miedos_element}>
                 <img src="/img/smartphone.png" alt="smartphone logo" />
 
                 {/* <svg id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 144.71 144.71"><defs><style>.cls-1{fill:none;}.cls-2{opacity:0.49;}.cls-3{fill:#ffbc00;}.cls-4{clip - path:url(#clip-path);}</style><clipPath id="clip-path" transform="translate(-157.41 -1856.43)"><rect class="cls-1" x="194.49" y="1893.65" width="70.55" height="70.54" /></clipPath></defs><circle class="cls-3" cx="72.36" cy="72.36" r="72.36" /></svg> */}
@@ -77,6 +77,35 @@ export default function Home() {
 
         </section>
         <section id={styles.beneficios}>
+          <div className={styles.container}>
+            <h2>
+              SIN EMBARGO, TE GUSTARÍA...
+            </h2>
+            <div className={styles.beneficios_elements}>
+              <div className={styles.benedificios_element}>
+                <h3>Calidad</h3>
+                <p>Crear contenido valioso
+                  empleando herramientas
+                  y programas de calidad.</p>
+              </div>
+
+              <div className={styles.beneficios_element}>
+                <h3>Originalidad</h3>
+                <p>Una estrategia capaz
+                  de brindarte contenido
+                  creativo y único.</p>
+              </div>
+
+              <div className={styles.beneficios_element}>
+                <h3>Mensaje</h3>
+                <p>Compartir contenido
+                  capaz de transmitir
+                  un mensaje poderoso.</p>
+              </div>
+
+            </div>
+
+          </div>
 
         </section>
         <section id={styles.quien_soy}>
