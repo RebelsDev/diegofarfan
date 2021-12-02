c941c4d64f31226430d37c413edb4db6ca52b9cf
Renzo Damian <renzo.damian.go@gmail.com>
2021-12-02 13:03:50 -0500

    add hero img

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
 import NavBar from './shared/navbar'
 
 export default function Home() {
@@ -10,7 +11,31 @@ export default function Home() {
       <nav>
         <NavBar />
       </nav>
+      <main>
+        <section id='Hero'>
+          <div className={styles.hero}>
+            <div className={styles.container}>
+              <div id={styles.hero_text}>
+                <h1>
+                  Posiciono a marcas mediante la creación de contenido audiovisual
+                </h1>
+                <h2>
+                  FOTOGRAFÍA · EDICIÓN · VIDEO
+                </h2>
+              </div>
 
+              <div id={styles.hero_img}>
+                <img src="/img/diegofarfan.png" alt="diego farfan" />
+              </div>
+
+              <div id={styles.button}>
+                <button>Hablemos</button>
+              </div>
+            </div>
+          </div>
+
+        </section>
+      </main>
     </div>
   )
 }
