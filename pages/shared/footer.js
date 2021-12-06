1b0f842a8472f279320b3bb2542d8bc7c5ee89cc
Renzo Damian <renzo.damian.go@gmail.com>
2021-12-06 12:53:26 -0500

    add footer style

--- pages/shared/footer.js
+++ pages/shared/footer.js
@@ -1,7 +1,18 @@
+import styles from '../../styles/Footer.module.scss'
+
 export default function Footer() {
     return (
-        <div className="footer">
+        <div className={styles.Footer}>
+            <div className={styles.container}>
 
+                <div className={styles.politicas}>
+                    <p>Política de privacidad</p>
+                    <p>Términos y condiciones</p>
+                </div>
+                <div className={styles.copyright}>
+                    <p>® 2021 - DIEGO FARFAN</p>
+                </div>
+            </div>
         </div>
     )
 }
