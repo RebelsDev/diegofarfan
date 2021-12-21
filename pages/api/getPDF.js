f128ce476f7c3ada7d60c9c82e457a4eaa2db08f
Renzo Damian <renzo.damian.go@gmail.com>
2021-12-21 09:56:54 -0500

    test pdf

--- pages/api/getPDF.js
+++ pages/api/getPDF.js
@@ -1,11 +1,11 @@
 import fs from 'fs'
 import path from 'path'
 
-const filePath = path.resolve('.', 'public/dummy.pdf')
+const filePath = path.resolve('.', 'public/Razones_por_las_que_tu_marca_necesita_fotografía_publicitaria-Diego_Farfan.pdf')
 const imageBuffer = fs.readFileSync(filePath)
 
 export default function (req, res) {
     res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'attachment; filename=dummy.pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename=Razones_por_las_que_tu_marca_necesita_fotografía_publicitaria-Diego_Farfan.pdf');
     res.send(imageBuffer)
 }
